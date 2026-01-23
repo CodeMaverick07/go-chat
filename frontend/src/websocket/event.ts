efa29bc9a4fe351f45f66c03b2569633b7032cee
CodeMaverick07 <hemantjatal59@gmail.com>
2026-01-23 23:41:42 +0530

    changes in event

--- frontend/src/websocket/event.ts
+++ frontend/src/websocket/event.ts
@@ -1,11 +1,10 @@
 export const WS_EVENTS = {
-  // (server -> client)
   EventNewMessage: "new_message",
   EventMessageDelivered: "message_delivered",
   EventMessageRead: "message_read",
   EventConversationCreated: "conversation_created",
   EventTypingIndicator: "typing_indicator",
-  // (client -> server)
+
   EventSendMessage: "send_message",
   EventCreateConversation: "create_conversation",
   EventGetConversations: "get_conversations",
